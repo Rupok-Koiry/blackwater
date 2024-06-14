63c2f74b570aaa5886737ac21e5a12615f5fb4d7
Rupok-Koiry <koiry.rupok@gmail.com>
2024-06-14 20:57:39 +0600

    Css Fixed

--- components/PreLoader.js
+++ components/PreLoader.js
@@ -1,26 +1,26 @@
 import React from "react";
 
 const PreLoader = () => {
-   return (
-      <div className={"container preloader"}>
-         <div className={"block"}></div>
-         <div className={"block"}></div>
-         <div className={"block"}></div>
-         <div className={"block"}></div>
-         <div className={"block"}></div>
-         <div className={"block"}></div>
-         <div className={"block"}></div>
-         <div className={"block"}></div>
-         <div className={"block"}></div>
-         <div className={"block"}></div>
-         <div className={"block"}></div>
-         <div className={"block"}></div>
-         <div className={"block"}></div>
-         <div className={"block"}></div>
-         <div className={"block"}></div>
-         <div className={"block"}></div>
-      </div>
-   );
+  return (
+    <div className={"container preloader"}>
+      <div className={"block"}></div>
+      <div className={"block"}></div>
+      <div className={"block"}></div>
+      <div className={"block"}></div>
+      <div className={"block"}></div>
+      <div className={"block"}></div>
+      <div className={"block"}></div>
+      <div className={"block"}></div>
+      <div className={"block"}></div>
+      <div className={"block"}></div>
+      <div className={"block"}></div>
+      <div className={"block"}></div>
+      <div className={"block"}></div>
+      <div className={"block"}></div>
+      <div className={"block"}></div>
+      <div className={"block"}></div>
+    </div>
+  );
 };
 
 export default PreLoader;
